69bfb3463417b9063c83ef1791b2f1c77b4271f0
cookTanxin <wenfuwork@gmail.com>
2025-05-01 12:40:22 +0800

    更新

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import { Heart } from '@/components/icons'
 const Footer = () => {
   return (
     <div className="flex items-center justify-center gap-1 mt-24 mb-16 text-sm font-medium text-zinc-500">
-     快乐 2025
+     2025
     </div>
   )
 }
