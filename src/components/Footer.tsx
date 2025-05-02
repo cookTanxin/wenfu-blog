302aad5794d63a56b86c85bd5a8b69f6dd29d50f
cookTanxin <wenfuwork@gmail.com>
2025-05-02 11:03:43 +0800

    update style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { Heart } from '@/components/icons'
 
 const Footer = () => {
   return (
-    <div className="flex items-center justify-center gap-1 mt-24 mb-16 text-sm font-medium text-zinc-500">
+    <div className="flex items-center justify-center gap-1 mt-10 mb-10 text-sm font-medium text-zinc-500">
      2025
     </div>
   )
